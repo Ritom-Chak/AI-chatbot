63e012811faedd44bd82ebc775b35cb1f91fb0d9
Ritom_chak <ritom.chakraborty@solidinspire.com>
2025-08-27 16:00:07 +0530

    Improved text handling

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
 import { FaArrowUp } from "react-icons/fa";
 import { Button } from "./ui/button"
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type FormData = {
     prompt: string;
@@ -12,19 +13,32 @@ type ChatResponse = {
     message: string;
 }
 
+type Message = {
+    role: 'user' | 'bot';
+    content: string;
+}
+
 const ChatBot = () => {
-    const [messages, setMessages] = useState<string[]>([]);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [isBotTyping, setIsBotTyping] = useState(false);
+    const formRef = useRef<HTMLFormElement | null>(null);
     const conversationId = useRef(crypto.randomUUID())
     const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+    useEffect(() => {
+        formRef.current?.scrollIntoView({behavior: 'smooth'})
+    }, [messages])
     
     const onSubmit = async ({ prompt }: FormData) => {
-        setMessages(prev => [...prev, prompt])
+        setMessages(prev => [...prev, { content: prompt, role: 'user' }])
+        setIsBotTyping(true);
         reset();
         const { data } = await axios.post<ChatResponse>('/api/chat', {
             prompt,
             conversationId: conversationId.current
         });
-        setMessages(prev => [...prev, data.message])
+        setMessages(prev => [...prev, { content: data.message, role: 'bot' }])
+        setIsBotTyping(false);
     }
 
     const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
@@ -35,14 +49,27 @@ const ChatBot = () => {
     }
     return (
         <div>
-            <div>
+            <div className='flex flex-col gap-3 mb-10'>
                 {messages.map((message, index) => (
-                    <p key ={index}>{message}</p>
+                    <p key={index} className={`px-3 py-1 rounded-xl ${
+                        message.role === 'user'
+                        ? 'bg-blue-600 text-white self-end'
+                        : 'bg-gray-100 text-black self-start'
+                        }`}>
+                        <ReactMarkdown>{message.content}</ReactMarkdown></p>
                 ))}
+                {isBotTyping && (
+                    <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl ">
+                    <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse"></div>
+                    <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.2s]"></div>
+                    <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]"></div>
+                    </div>
+                )}
             </div>
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 onKeyDown={onKeyDown}
+                ref={formRef}
                 className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
                 <textarea
                     {...register('prompt',
