36de93f0c582e84286d62e80975df0527dd6c0b4
Ritom_chak <ritom.chakraborty@solidinspire.com>
2025-08-27 12:24:16 +0530

    Setup routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,22 +1,12 @@
 import express from "express";
-import type { Request, Response } from "express";
-import z from "zod";
-import { chatService } from "./services/chat.service";
+import router from "./routes";
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+app.use(router);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World! Server is running.");
-});
+const port = process.env.PORT || 3000;
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello from the API!" });
-});
-
-app.post("/api/chat", async (req: Request, res: Response) => {});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
