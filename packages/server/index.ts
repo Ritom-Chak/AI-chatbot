003ff9f99fe8afddab61e5c9e6b57d8794bb62c1
Ritom_chak <ritom.chakraborty@solidinspire.com>
2025-08-27 12:13:19 +0530

    Refactored code and setup OpenAI

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import type { Request, Response } from "express";
+import z from "zod";
+import { chatService } from "./services/chat.service";
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
@@ -11,6 +14,8 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello from the API!" });
 });
+
+app.post("/api/chat", async (req: Request, res: Response) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
